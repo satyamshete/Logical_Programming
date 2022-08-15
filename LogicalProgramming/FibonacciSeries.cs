17cd25b239f28419568cebf87c31f6a777b396fd
satyamshete <shetesatyam99@gmail.com>
2022-08-15 19:52:25 +0530

    [Satyam]: UC1 FibonacciSeries

---
+++ LogicalProgramming/FibonacciSeries.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FibonacciSeries
+    {
+        public FibonacciSeries()
+        {
+            Console.WriteLine("Enter Numbers to be print by Fibonacci Series");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            Console.WriteLine("First " + input + " Numbers form Fibonacci Series are");
+            for (int i = 1; i <= input; i++)
+            {
+                if (i == 1)
+                {
+                    Console.Write(a + " ");
+                }
+                else if (i == 2)
+                {
+                    Console.Write(b + " ");
+                }
+                else
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    Console.Write(c + " ");
+                }
+            }
+        }
+    }
+
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Logical Programming
+            Console.WriteLine("Logical Programming");
+            Console.WriteLine("1. Fibonacci Series");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            switch (answer)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Give proper selection");
+                    break;
+
+            }
+        }
+    }
+}
