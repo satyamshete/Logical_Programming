e6375b08f3d0553364489e9c71b08e70c95311dd
satyamshete <shetesatyam99@gmail.com>
2022-08-15 20:01:06 +0530

    [Satyam]: UC2 PerfectNumber

---
+++ LogicalProgramming/PerfectNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PerfectNumber
+    {
+        public bool Factors(int Number, int potential_factor)
+        {
+            if (Number % potential_factor == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public PerfectNumber()
+        {
+            int check = 0;
+            Console.WriteLine("Enter number to check whether it is Perfect or not");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number / 2; i++)
+            {
+                if (Factors(number, i))
+                {
+                    check += i;
+                }
+            }
+            if (check == number)
+            {
+                Console.WriteLine("number is perfect");
+            }
+            if (check != number)
+            {
+                Console.WriteLine("addition of all factors is: " + check);
+                Console.WriteLine("number isnot perfect");
+            }
+        }
+    }
+}
--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -7,11 +7,15 @@
             //Logical Programming
             Console.WriteLine("Logical Programming");
             Console.WriteLine("1. Fibonacci Series");
+            Console.WriteLine("2. Perfect Number");
             int answer = Convert.ToInt32(Console.ReadLine());
             switch (answer)
             {
                 case 1:
                     FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNumber perfect = new PerfectNumber();
                     break;
                 default:
                     Console.WriteLine("Give proper selection");
